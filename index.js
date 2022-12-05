a84b491cb0bc264bef54630587c13943cfa5d5df
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2022-12-06 00:12:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(username,useremail){
+   return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        username,
+        useremail
+    })
+   })
+   .then(function(response){
+    return response.json();
+   })
+   .then(function(object){
+    document.body.innerHTML = object["id"]
+   })
+   .catch(function(error){
+    document.body.innerHTML = error.message
+   })
+}
